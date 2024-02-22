68211f8f245a117c2a1e2fc173fa4c2998106219
Peter Škríba <peter.skriba@gmail.com>
2024-02-22 10:12:02 +0100

    chore: removing java directory for submodule

--- src/java/scripts/README.md
+++
@@ -1,5 +0,0 @@
-# Scripts
-
-```bash
-chmod +x *.sh
-```
--- src/java/scripts/build-all.sh
+++
@@ -1,6 +0,0 @@
-./build-mac-arm.sh
-echo "Successfully built mac-arm"
-./build-mac-intel.sh
-echo "Successfully built mac-intel"
-./build-win.sh
-echo "Successfully built win"
