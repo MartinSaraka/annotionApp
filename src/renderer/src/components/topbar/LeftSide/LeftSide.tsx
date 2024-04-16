b84414f28d4f79a893cbcbb0c309003940d5580b
Karolína Tóthová <56071209+ktothova@users.noreply.github.com>
2024-04-16 18:54:20 +0200

    Update LeftSide.tsx
    added "help" button

--- src/renderer/src/components/topbar/LeftSide/LeftSide.tsx
+++ src/renderer/src/components/topbar/LeftSide/LeftSide.tsx
@@ -1,25 +1,147 @@
-import { memo } from 'react'
-import { type ComponentProps } from '@stitches/react'
-import { useTranslation } from 'react-i18next'
+import { memo, useState } from 'react';
+import { type ComponentProps } from '@stitches/react';
+import { useTranslation } from 'react-i18next';
 
-import { Box, Chip, DropdownMenu, Icon, Text } from '@renderer/ui'
-import { useImageStore } from '@renderer/store'
+import { Box, Chip, DropdownMenu, Icon, Text, Button } from '@renderer/ui';
+import { useImageStore } from '@renderer/store';
+import logo from '../../../../../../resources/logo-icon.svg';
 
-import logo from '../../../../../../resources/logo-icon.svg'
-
-type TTopBarLeftSideProps = ComponentProps<typeof Box>
+type TTopBarLeftSideProps = ComponentProps<typeof Box>;
 
 const LeftSide = ({ css, ...rest }: TTopBarLeftSideProps) => {
-  const { t } = useTranslation('common')
+  const { t } = useTranslation('common');
+  const data = useImageStore((state) => state.getData());
+  const [showOnboarding, setShowOnboarding] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 3;
 
-  const data = useImageStore((state) => state.getData())
+  const goToNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const goToPrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const toggleOnboarding = () => setShowOnboarding(!showOnboarding);
 
   return (
     <Box
-      aria-description={t('aria.description.imageInfo')}
-      css={{ flexDirection: 'row', alignItems: 'center', gap: '$3', ...css }}
-      {...rest}
+  aria-description={t('aria.description.imageInfo')}
+  css={{ flexDirection: 'row', alignItems: 'center', gap: '$3', ...css }}
+  {...rest}
+>
+{showOnboarding && (
+    <div
+      style={{
+        position: 'fixed',
+        top: '0',
+        left: '0',
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent black background
+        zIndex: '999', // Ensure it is above other content but below the modal
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'white'
+      }}
+      onClick={toggleOnboarding} // Close the onboarding card when clicking on the background
     >
+      <Box
+        css={{
+          width: '400px',
+          height: '300px',
+          backgroundColor: '#000', // Black background
+          padding: '20px',
+          borderRadius: '8px',
+          boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.5)',
+          color: '#fff',
+          zIndex: '1000', // Ensure it is above other content
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+        onClick={(e) => e.stopPropagation()} // Prevent clicks inside the card from closing it
+      >
+          <Box css={{
+            flex: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+                        alignItems: 'center',
+            margin: '10px'
+          }}>
+            {currentPage === 1 && (
+              <>
+                          <Text
+                            variant="md"
+                            css={{
+                              fontSize: '24px',
+                              fontWeight: 'bold',
+                              marginBottom: '8px'
+                            }}
+                          >
+                            Left side bar
+                          </Text>
+                          <Text css={{ fontSize: '16px' }}>
+                            On the left side you can see all of your annotations.
+                          </Text>
+                        </>
+                      )}
+                      {currentPage === 2 && (
+                        <>
+                          <Text
+                            variant="lg"
+                            css={{
+                              fontSize: '24px',
+                              fontWeight: 'bold',
+                              marginBottom: '8px'
+                            }}
+                          >
+                            Top side bar
+                          </Text>
+                          <Text css={{ fontSize: '16px' }}>
+                            The top side bar contains all tools necessary for annotating.
+                          </Text>
+              </>
+            )}
+            {currentPage === 3 && (
+              <>
+                          <Text
+                            variant="lg"
+                            css={{
+                              fontSize: '24px',
+                              fontWeight: 'bold',
+                              marginBottom: '8px'
+                            }}
+                          >
+                            Right side bar
+                          </Text>
+                          <Text css={{ fontSize: '16px' }}>
+                            On the right side, you can see the differenct classes and parameters. After clicking on an annotation, you will see the features of the annotation and AI rules.
+                          </Text>
+              </>
+            )}
+        </Box>
+
+        <Box css={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Button css={{ margin: '5px' }} onClick={(e) => { e.stopPropagation(); goToPrevPage(); }} disabled={currentPage === 1}>Previous</Button>
+          <Button css={{ margin: '5px' }} onClick={(e) => { e.stopPropagation(); goToNextPage(); }} disabled={currentPage === totalPages}>Next</Button>
+        </Box>
+        <Button ghost onClick={toggleOnboarding} css={{ marginTop: '20px' }}>Close</Button>
+      </Box>
+    </div>
+
+
+  )}
+<Button onClick={toggleOnboarding}>Help</Button>
+
+
       <DropdownMenu.Root>
         <DropdownMenu.Trigger
           css={{
@@ -47,7 +169,6 @@ const LeftSide = ({ css, ...rest }: TTopBarLeftSideProps) => {
               css={{ filter: 'drop-shadow(0 0 1px $colors$dark1)' }}
             />
           </Box>
-
           <Icon
             name="ChevronDownIcon"
             width={15}
@@ -62,31 +183,14 @@ const LeftSide = ({ css, ...rest }: TTopBarLeftSideProps) => {
       </DropdownMenu.Root>
 
       <Box css={{ flexDirection: 'row', gap: '$1' }}>
-        <Text
-          variant="lg"
-          css={{ color: '$dark4' }}
-          aria-describedby="image-project"
-        >
-          Personal
-        </Text>
-        <Text role="separator" variant="lg" css={{ color: '$dark4' }}>
-          /
-        </Text>
-        <Text
-          as="h1"
-          variant="lg"
-          css={{ color: '$light' }}
-          aria-describedby="image-name"
-        >
-          {data?.filename}
-        </Text>
+        <Text variant="lg" css={{ color: '$dark4' }} aria-describedby="image-project">Personal</Text>
+        <Text role="separator" variant="lg" css={{ color: '$dark4' }}>/</Text>
+        <Text as="h1" variant="lg" css={{ color: '$light' }} aria-describedby="image-name">{data?.filename}</Text>
       </Box>
 
-      <Chip small aria-describedby="image-format">
-        {data?.format}
-      </Chip>
+      <Chip small aria-describedby="image-format">{data?.format}</Chip>
     </Box>
-  )
-}
+  );
+};
 
-export default memo(LeftSide) as typeof LeftSide
+export default memo(LeftSide);
